79681c4a1897ac13c216eb3e28ecbeb9eb5125fc
wajdiAb <wagde.abo164@gmail.com>
2025-07-10 13:21:19 +0300

    matrix rotate sol

--- katas/matrix_rotate.py
+++ katas/matrix_rotate.py
@@ -5,8 +5,15 @@ def rotate_matrix(matrix):
     Args:
         matrix: the 2D square matrix to rotate
     """
-    pass
+    n = len(matrix)
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
 
 def print_matrix(matrix):
     """
