6b4eeafab213e04e0cb2e15397b35b02ceb47967
wajdiAb <wagde.abo164@gmail.com>
2025-07-20 17:34:05 +0300

    word count with test sol

--- katas/word_count.py
+++ katas/word_count.py
@@ -8,7 +8,8 @@ def count_words(sentence):
     Returns:
         the number of words in the sentence
     """
-    return 0
+    words = sentence.split()
+    return len(words)
 
 
 if __name__ == '__main__':
